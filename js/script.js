c6f92144db8b82da18c8cabe0102155692b3a3cb
Chelsey Dolan <chelseymarie6@gmail.com>
2020-12-20 22:04:15 -0800

    chore: updaing script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 (function() {
 	const url = "http://api.openweathermap.org/data/2.5/weather?q=";
-	const apiKey = "f1a3c3e23b73b9d991771e6b03bf849f"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+	const apiKey = "APIKEY"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
 	const activities = {
 		teamIn: ['basketball','hockey','volleyball'],
 		teamOutWarm: ['softball/baseball','football/soccer','American football','rowing','tennis','volleyball','ultimate frisbee','rugby'],
